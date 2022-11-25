c9ca8e43f96e3c3a369fca83bfaa15d88ea16c60
Sarathharidas <30758858+Sarathharidas@users.noreply.github.com>
2022-11-24 19:53:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,8 +27,8 @@ def hello_world():
   return render_template("start.html")
 
 
-# @app.route('/translate',methods=["GET"])
-# def downloadFile ():
+@app.route('/translate',methods=["GET"])
+def downloadFile ():
   # #youtube_link =request.args.get('youtube-link')
   # #with youtube_dl.YoutubeDL(ydl_opts) as ydl:
       # #ydl.download([youtube_link])
@@ -40,11 +40,11 @@ def hello_world():
   # path_to_html = os.path.abspath("index.html")
   # # return render_template('index.html')
   # # return send_from_directory(os.getcwd(), "content/myfile.txt", as_attachment=True)
-  # subtitles = open(path_name, "r").read().split("\n\n")
-  # for i in subtitles:
-    # text_array+=i.split("\n")
-  # print(text_array)
-  # return  render_template("index.html",text=text_array)
+  subtitles = open(path_name, "r").read().split("\n\n")
+  for i in subtitles:
+    text_array+=i.split("\n")
+  print(text_array)
+  return  render_template("index.html",text=text_array)
     # #For windows you need to use drive name [ex: F:/Example.pdf]
     # # path =
     # # return send_file(path, as_attachment=True)
